609e2bf5992a5f766c841892b4f0f7828a3a2869
MahakSharma04 <mahaksharma1104@gmail.com>
2025-07-10 16:43:35 +0530

    commit

--- server.js
+++ server.js
@@ -11,20 +11,20 @@ app.use(express.static("public"));
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
-//   app.listen(8080, () => {
-//     console.log("Server started");
-//   });
-// });
-
-mongoose
-  .connect(
-  `mongodb+srv://${dbuser}:${dbpass}@cluster0.fhfiwmd.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
-  )
-  .then(() => {
-    app.listen(8080, () => {
-      console.log("Server started");
-    });
+mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+  app.listen(8080, () => {
+    console.log("Server started");
   });
+});
+
+// mongoose
+//   .connect(
+//   `mongodb+srv://${dbuser}:${dbpass}@cluster0.fhfiwmd.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+//   )
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
 
 app.use("/api/users", userRouter);
