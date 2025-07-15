4ca73bfe5693ae6bb491f748872b41eb582b6c0e
MahakSharma04 <mahaksharma1104@gmail.com>
2025-07-15 14:46:25 +0530

    commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authenticate, authorize } from "../middlewares/auth.js";
+//import { authenticate, authorize } from "../middlewares/auth.js";
 const Router = express.Router();
 import {
   register,
@@ -13,11 +13,11 @@ import {
 } from "../controllers/userController.js";
 Router.post("/register", register);
 Router.post("/login", login);
-Router.get("/", authenticate, authorize("admin"), showUsers);
-Router.get("/:id", authenticate, authorize("admin"), getUser);
-Router.patch("/:id", authenticate, authorize("admin"), updateUser);
-Router.delete("/:id", authenticate, authorize("admin"), deleteUser);
-Router.get("/:id/profile", authenticate, profile);
-Router.patch("/:id/profile", authenticate, updateProfile);
+Router.get("/", showUsers);
+Router.get("/:id", getUser);
+Router.patch("/:id", updateUser);
+Router.delete("/:id", deleteUser);
+Router.get("/:id/profile", profile);
+Router.patch("/:id/profile", updateProfile);
 
 export default Router;
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.static("public"));
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
@@ -17,7 +20,7 @@ const dbpass = encodeURIComponent(process.env.DBPASS);
 
 mongoose
   .connect(
-    `mongodb+srv://${dbuser}:${dbpass}@cluster0.qjxhv.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb+srv://${dbuser}:${dbpass}@cluster0.fhfiwmd.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
     app.listen(8080, () => {
@@ -26,3 +29,4 @@ mongoose
   });
 
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
